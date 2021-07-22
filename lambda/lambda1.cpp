d6ba0d61cfaf41e28153df718878140b0b983028
llzen44 <73878466+llzen44@users.noreply.github.com>
2021-07-22 09:50:13 +0200

    Create lambda1.cpp

---
+++ lambda/lambda1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void log (int a){
+    cout << "a = " << a << endl;
+}
+
+int main() {
+    int a = 42;
+    log(a);
+    
+    [a](){
+        cout << "lambda with capture, a = " << a << endl;
+        //a++;
+    }();
+    log(a);
+
+    [](int a){
+        cout << "lambda with parameter, a = " << a << endl;
+        a++;
+    }(a);
+    log(a);
+
+    [](int* a){
+        cout << "lambda with parameter pointer, a = " << *a << endl;
+        (*a)++;
+    }(&a);
+    log(a);
+
+    auto lambda = [](int& a){
+        cout << "lambda with parameter reference, a = " << a << endl;
+        a++;
+    };
+    lambda(a);
+    log(a);
+    
+    int* ptr_a = &a;
+    [ptr_a](){
+        cout << "lambda with capture pointer, a = " << *ptr_a << endl;
+        (*ptr_a)++;
+    }();
+    log(a);
+    
+    return 0;
+}
